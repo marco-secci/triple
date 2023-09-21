fcb6d31dd2421b77e59a0d8c31012797ae1d5d77
CecicOic <marcosecci2002@gmail.com>
2023-09-21 22:54:28 +0200

    skeleton

---
+++ backend/data_analysis/advanced_metrics.py
@@ -0,0 +1,22 @@
+# Imports
+import pandas as pd
+import numpy as np
+
+
+class AdvancedMetrics:
+    def __init__(self, basic_stats):
+        self.basic_stats = basic_stats  # You can initialize it with basic statistics like points, rebounds, etc.
+
+    def calculate_PER(self):
+        # Calculate Player Efficiency Rating (PER)
+        pass
+
+    def calculate_VORP(self):
+        # Calculate Value Over Replacement Player (VORP)
+        pass
+
+    def calculate_AST_TO_ratio(self):
+        # Calculate Assist to Turnover Ratio
+        pass
+
+    # Add more methods for other advanced metrics here
