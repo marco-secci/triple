e46cc3728efc10c202ef66d7e35f0814d9120019
Marco Secci <marcosecci2002@gmail.com>
2023-10-08 11:52:14 +0200

    changed methods to work with both season and history-career, using the optional parameter `season`.

--- backend/database_utils/stored_procedures.py
+++ backend/database_utils/stored_procedures.py
@@ -24,40 +24,39 @@ class StoredProcedures:
         to connect to the database."""
         self.conn = connect_to_db()
 
-    # =========== #
-    # INIT METHOD #
-    # =========== #
+    # ================ #
+    # __ENTER__ METHOD #
+    # ================ #
     def __enter__(self):
         """Ran when the class is initialized using `with`."""
         return self
 
-    # =========== #
-    # INIT METHOD #
-    # =========== #
+    # =============== #
+    # __EXIT__ METHOD #
+    # =============== #
     def __exit__(self, exc_type, exc_value, traceback):
         """Ran when the class is initialized using `with` and the execution is leaving the block."""
         self.conn.close()
 
-    # ======================== #
-    # PLAYER CAREER AVG METHOD #
-    # ======================== #
-    def get_player_career_avg(self, player_id):
-        """### Parameters : `player_id (int)`
-        The method takes as input the `id` of a player and fetches all games he or she played, all
-        their statlines and calculates the average statline produced by them.
-
+    # ================== #
+    # PLAYER AVG METHOD #
+    # ================= #
+    def get_player_avg(self, player_id, season=None):
+        """### Parameters:
+        -`player_id`: `int`
+        - `season = None`: `int` >= 1946
+        This method returns as a `list` the averages in every base stat of a single player during a single season or their entire
+        career, if a `season` parameter is not provided.
         #### IN CASE OF `NULL`:
-        Every average stat is calculated on the number of statlines including that particular statistic;
-        for example, Wilt Chamberlain played in an era where blocks were not tracked, but there are some
-        unofficial tracked statlines of his that include blocks. His career block average will be calculated
-        on the total number of statlines including blocks and not on his total number of games.
-
-        This means that `NULL` values are NOT treated as `0`, and statlines including `NULL` values are NOT
-        automatically excluded from the calculation of the averages.
+        `NULL` values are NOT treated as `0`, and statlines including `NULL` values are NOT
+        automatically excluded from the calculation of the averages. Instead, every statistic average will be
+        calculated with the number of available statlines containing that particular stat.
         """
-
         cur = self.conn.cursor()
-        cur.callproc("player_career_avg", (player_id))
+        if season is not None:
+            cur.callproc("player_season_avg", (player_id, season))
+        else:
+            cur.callproc("player_career_avg", (player_id, None))
 
         # Initializing variable as a list where the final output will be appended:
         results = []
@@ -68,19 +67,26 @@ class StoredProcedures:
 
         return results
 
-    # ======================== #
-    # PLAYER SEASON AVG METHOD #
-    # ======================== #
-    def get_player_season_avg(self, player_id, season):
-        """### Parameters: `player_id` int, `season` int >= 1946
-        This method returns as a `list` the averages in every base stat of a single player during a single season.
+    # =============== #
+    # TEAM AVG METHOD #
+    # =============== #
+    def get_team_avg(self, team_id, season=None):
+        """### Parameters:
+        - `team_id`: `int`
+        - `season = None` `int` >= 1946
+        This method returns as a `list` the averages in every base stat of the given team during the course of the selected season,
+        or for its entire existence if a `season` parameter is not provided.
         #### IN CASE OF `NULL`:
         `NULL` values are NOT treated as `0`, and statlines including `NULL` values are NOT
         automatically excluded from the calculation of the averages. Instead, every statistic average will be
         calculated with the number of available statlines containing that particular stat.
         """
+
         cur = self.conn.cursor()
-        cur.callproc("player_season_avg", (player_id, season))
+        if season is not None:
+            cur.callproc("team_season_avg", (team_id, season))
+        else:
+            cur.callproc("team_alltime_avg", (team_id, None))
 
         # Initializing variable as a list where the final output will be appended:
         results = []
@@ -91,45 +97,25 @@ class StoredProcedures:
 
         return results
 
-    # ====================== #
-    # TEAM SEASON AVG METHOD #
-    # ====================== #
-    def get_team_season_avg(self, team_id, season):
-        """### Parameters: `team_id` int, `season` int >= 1946
-        This method returns as a `list` the averages in every base stat of a single player during a single season.
-        #### IN CASE OF `NULL`:
-        `NULL` values are NOT treated as `0`, and statlines including `NULL` values are NOT
-        automatically excluded from the calculation of the averages. Instead, every statistic average will be
-        calculated with the number of available statlines containing that particular stat.
-
-        ###### TODO: solve some problems regarding database tables identification
-        """
-
-        cur = self.conn.cursor()
-        cur.callproc("team_season_avg", (team_id, season))
-
-        # Initializing variable as a list where the final output will be appended:
-        results = []
-        for result in cur.stored_results():
-            results.append(result.fetchall())
-
-        cur.close()
-
-        return results
-
-    # ======================== #
-    # LEAGUE SEASON AVG METHOD #
-    # ======================== #
-    def get_league_season_avg(self, league_id, season):
-        """### Parameters: `league_id` str, `season` int >= 1946
-        This method returns as a `list` the averages in every base stat of a single player during a single season.
+    # ================= #
+    # LEAGUE AVG METHOD #
+    # ================= #
+    def get_league_avg(self, league_id, season=None):
+        """### Parameters:
+        - `league_id`: `str` similar to ITA1, SPA3, etc. matching the national level
+        - `season) None`: `int` >= 1946
+        This method returns as a `list` the averages in every base stat of a league during a single season or its entire
+        existence if `season = None`.
         #### IN CASE OF `NULL`:
         `NULL` values are NOT treated as `0`, and statlines including `NULL` values are NOT
         automatically excluded from the calculation of the averages. Instead, every statistic average will be
         calculated with the number of available statlines containing that particular stat.
         """
         cur = self.conn.cursor()
-        cur.callproc("league_season_avg", (league_id, season))
+        if season is not None:
+            cur.callproc("league_season_avg", (league_id, season))
+        else:
+            cur.callproc("league_alltime_avg", (league_id, None))
 
         # Initializing variable as a list where the final output will be appended:
         results = []
